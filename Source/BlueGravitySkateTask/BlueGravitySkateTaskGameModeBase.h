dbd43288186f5e1765b99be81f8e60213e0d6521
BenjaminGomez <benjamingomez@ravegan.com>
2024-06-05 17:06:48 -0300

    Unreal project initial setup

---
+++ Source/BlueGravitySkateTask/BlueGravitySkateTaskGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BlueGravitySkateTaskGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BLUEGRAVITYSKATETASK_API ABlueGravitySkateTaskGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
