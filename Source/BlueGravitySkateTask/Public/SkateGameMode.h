7f2a8f076bc1c5c9e9eda05c33b54c99fec75ca0
BenjaminGomez <benjamingomez@ravegan.com>
2024-06-05 21:36:57 -0300

    Added custom game mode to set default pawn and a level to test for now

---
+++ Source/BlueGravitySkateTask/Public/SkateGameMode.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SkateGameMode.generated.h"
+
+UCLASS()
+class BLUEGRAVITYSKATETASK_API ASkateGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
